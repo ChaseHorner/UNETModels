e63565c773b183db7f3b574d511d4af03cae004e
Charles Horner <c710h797@ku.edu>
2025-10-21 17:04:32 -0500

    save_resfs

---
+++ save_resfs.py
@@ -0,0 +1,17 @@
+import os
+
+
+output_folder = '/resfs/GROUPS/KBS/kars_yield/model_outputs'
+
+def save_resfs(model_folder, model_name):
+    if not os.path.exists(output_folder + model_name):
+        os.makedirs(output_folder + model_name)
+        
+    for filename in os.listdir(model_folder):
+        if filename.endswith('.pt'):
+            continue
+        src = os.path.join(model_folder, filename)
+        dst = os.path.join(output_folder + model_name, filename)
+        if os.path.isfile(src):
+            with open(src, 'rb') as fsrc, open(dst, 'wb') as fdst:
+                fdst.write(fsrc.read())
