c835c66c7c60548f6c4e97e5773d8c28b49cad48
Chase <c710h797@ku.edu>
2025-11-04 16:36:03 -0600

    don't save slurm reports

--- save_resfs.py
+++ save_resfs.py
@@ -6,7 +6,7 @@ output_folder = '/resfs/GROUPS/KBS/kars_yield/model_outputs'
 def save_resfs(model_folder, model_name):
         
     for filename in os.listdir(model_folder):
-        if filename.endswith('.pt'):
+        if filename.endswith('.pt') or filename.endswith('.out') or filename.endswith('.err'):
             continue
         src = os.path.join(model_folder, filename)
         dst = os.path.join(output_folder, model_name, filename)
